d34f002fa575026cedf8b336d6275e14c6f3bb6c
RM Subhash Sanjeewa <subhash@crowderia.com>
2017-06-20 10:26:54 +0530

    Initial Commit

---
+++ chatterhelper/chatterhelper.h
@@ -0,0 +1,19 @@
+//
+//  chatterhelper.h
+//  chatterhelper
+//
+//  Created by RM Subhash Sanjeewa on 6/20/17.
+//  Copyright © 2017 RM Subhash Sanjeewa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for chatterhelper.
+FOUNDATION_EXPORT double chatterhelperVersionNumber;
+
+//! Project version string for chatterhelper.
+FOUNDATION_EXPORT const unsigned char chatterhelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <chatterhelper/PublicHeader.h>
+
+
