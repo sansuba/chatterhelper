18c3260a2aba1007a278b8ce3e6a60bb44ef4d00
RM Subhash Sanjeewa <subhash@crowderia.com>
2017-06-20 11:04:43 +0530

    updated

---
+++ chatterhelper/Calculate.swift
@@ -0,0 +1,13 @@
+//
+//  Calculate.swift
+//  chatterhelper
+//
+//  Created by RM Subhash Sanjeewa on 6/20/17.
+//  Copyright © 2017 RM Subhash Sanjeewa. All rights reserved.
+//
+
+import UIKit
+
+class Calculate: NSObject {
+
+}
